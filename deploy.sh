bcbf1ccbfcf5d9ff858965b6c283cba1ba1b6e50
Reshma Khilnani <reshma.khilnani@gmail.com>
2020-03-27 15:07:07 -0700

    Content updates and new pages

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
   # Copy the files to the S3 bucket
   aws s3 cp index.html s3://identify-immune-www/
+  aws s3 cp success.html s3://identify-immune-www/
+  aws s3 cp error.html s3://identify-immune-www/
