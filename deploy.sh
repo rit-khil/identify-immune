6e9e99ed1ac93738d7558dca875d8eb6740f6db9
Reshma Khilnani <reshma.khilnani@gmail.com>
2020-03-27 20:06:06 -0700

    Updating backend

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
   # Copy the files to the S3 bucket
-  aws s3 cp index.html s3://identify-immune-www/
-  aws s3 cp success.html s3://identify-immune-www/
-  aws s3 cp error.html s3://identify-immune-www/
+  aws s3 cp index.html s3://identify-immune-www/ --acl public-read
+  aws s3 cp success.html s3://identify-immune-www/ --acl public-read
+  aws s3 cp error.html s3://identify-immune-www/ --acl public-read
+
+# Invalidate cloudfront
+aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/index.html"
